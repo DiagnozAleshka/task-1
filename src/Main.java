1d79f0b1796805cf4602ec2583399af55966421e
DiagnozAleshka <Alekseypsih18@gmail.com>
2024-03-15 17:51:59 +0500

    fist

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int x = -25046;
+       int c = 20;
+       if (x>0){
+           System.out.println("колличество бонусных миль = " + x/c);
+       } else {
+           System.out.println("введите число больше нуля");
+       }
+
+    }
+}
